a392af9ab995a1e4505ee5ee27e910320bf98672
Fan Zeng <fanzengau@gmail.com>
2019-12-27 23:11:20 +1000

    375 ac

--- 375/375_Guess_Number_Higher_or_Lower_II.py
+++ 375/375_Guess_Number_Higher_or_Lower_II.py
@@ -1,63 +1,36 @@
 class Solution(object):
-	def getMoneyAmount(self, n):
-		if n == 1:
-			return 0
-		elif n == 2:
-			return 1
-		elif n == 3:
-			return 2
-		else:
+	def __init__(self):
+		self.dictRes = {}
 
-strat = {}
-strat[1] = 0
-strat[2] = 1
-def getStrategyDP(l, r):
-	if strat.get([r - 1]) is not None:
-		strat[n] = min(1 + )
-	if r - l == 1:
-		return l
-	elif r - l == 2:
-		return l + 1
-	else:
-		return min(l + strat)
-# 		sum = 0
-# 		if n == 1:
-# 			return 0
-# 		l = 1
-# 		r = n
-# 		m = (l + r) / 2
-#
-# 		while (l < r):
-# 			print 'm =', m
-# 			g = guess(m, n)
-# 			sum += m
-# 			if g == 0 or m == n - 1:
-# 				break
-# 			elif g > 0:
-# 				l = m
-# 				if m != n - 2:
-# 					r += 1
-# 					m = (l + r) / 2
-# 				else:
-# 					m = n -1
-# 			else:
-# 				r = m
-# 				m = (l + r) / 2
-# 		return sum
-#
-# def guess(num, ans):
-# 	if num > ans:
-# 		return -1
-# 	elif num < ans:
-# 		return 1
-# 	else:
-# 		return 0
-# strategy = {}
-# strategy[1] = 0
-# strategy[2] = 0
-# strategy[3] = 1
-# strategy[4] =
-# def get_strategy(l, r):
-# 	return l + strategy[r - l]
-test = Solution()
-print test.getMoneyAmount(10)
+	def getMoneyAmount(self, n):
+		return self.getMoneyAmountDP(1, n)
+
+
+	def getMoneyAmountDP(self, lo, hi):
+		gap = hi - lo
+		if gap <= 0:
+			res = 0
+		elif gap == 1:
+			res = lo
+		elif gap == 2:
+			res = lo + 1
+		else:
+			dictTrial = self.dictRes.get((lo, hi))
+			if dictTrial is not None:
+				res = dictTrial
+			else:
+				guess = lo
+				res = guess + self.getMoneyAmountDP(guess + 1, hi)
+				for guess in range(lo+1, hi):
+					maxLoss = max(self.getMoneyAmountDP(guess + 1, hi), self.getMoneyAmountDP(lo, guess - 1))
+					res = min(res, guess + maxLoss)
+		self.dictRes[(lo, hi)] = res
+		return res
+
+def doTest(n):
+	test = Solution()
+	print 'n =', n, ', result =', test.getMoneyAmount(i)
+
+if __name__ == '__main__':
+	for i in range(10):
+		doTest(i)
