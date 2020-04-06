facae6b39856f2b0ae0142f4038d5c194b0f6dd1
Fan Zeng <fanzengau@gmail.com>
2020-04-06 23:36:38 +1000

    121 ac

--- 121/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -4,6 +4,36 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        if len(prices) < 2:
+            return 0
+        diff = []
+        for i in xrange(len(prices)-1):
+            diff.append(prices[i+1]-prices[i])
+        max_sub_array = self.maxSubArray(diff)
+        if max_sub_array > 0:
+            return max_sub_array
+        else:
+            return 0
+
+    def maxSubArray(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            sum_since_reset = nums[0]
+            max_sum = sum_since_reset
+            for i in range(1, len(nums)):
+                sum_since_reset += nums[i]
+                if nums[i] > sum_since_reset:
+                    sum_since_reset = nums[i]
+                max_sum = max(max_sum, sum_since_reset)
+            return max_sum
+
+test = Solution()
+print test.maxProfit([7,1,5,3,6,4])
+print test.maxProfit([7,6,4,3,1])
+print test.maxProfit([7])
+print test.maxProfit([])
+
 
 # Say you have an array for which the ith element is the price of a given stock on day i.
 #
