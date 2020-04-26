00e650a95476fb8c1f8cf6cf84411d5f2c312e57
Fan Zeng <fanzengau@gmail.com>
2020-04-26 23:15:33 +1000

    108 ac

--- 108/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
+++ 108/108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def sortedArrayToBST(self, nums):
@@ -11,68 +11,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
+        if nums is None or len(nums) == 0:
+            return None
+        m = len(nums)/2
+        n = TreeNode(nums[m])
+        if m == 0:
+            n.left = None
+        else:
+            n.left = self.sortedArrayToBST(nums[:m])
+        if m + 1 == len(nums):
+            n.right = None
+        else:
+            n.right = self.sortedArrayToBST(nums[m+1:])
+        return n
 
-#     Given
-#     an
-#     array
-#     where
-#     elements
-#     are
-#     sorted in ascending
-#     order, convert
-#     it
-#     to
-#     a
-#     height
-#     balanced
-#     BST.
-#
-#     For
-#     this
-#     problem, a
-#     height - balanced
-#     binary
-#     tree is defined as a
-#     binary
-#     tree in which
-#     the
-#     depth
-#     of
-#     the
-#     two
-#     subtrees
-#     of
-#     every
-#     node
-#     never
-#     differ
-#     by
-#     more
-#     than
-#     1.
-#
-#     Example:
-#
-#     Given
-#     the
-#     sorted
-#     array: [-10, -3, 0, 5, 9],
-#
-#     One
-#     possible
-#     answer is: [0, -3, 9, -10, null, 5], which
-#     represents
-#     the
-#     following
-#     height
-#     balanced
-#     BST:
-#
-#     0
-#
-# / \
-#     -3
-# 9
-# / /
-# -10
-# 5
+def tree2Str(root):
+    n = root
+    s = ''
+    if n is not None:
+        s += str(n.val) + '\n'
+        s_left = tree2Str(root.left)
+        if s_left is not None and len(s_left) > 0:
+            s += 'left = ' + s_left
+        s_right = tree2Str(root.right)
+        if s_right is not None and len(s_right) > 0:
+            s += 'right = ' + s_right
+    return s
+
+test = Solution()
+print tree2Str(test.sortedArrayToBST([-10,-3,0,5,9]))
