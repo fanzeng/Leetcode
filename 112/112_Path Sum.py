5cd2a9e88135078bf5009e9c06316408904f7a83
Fan Zeng <fanzengau@gmail.com>
2020-04-26 23:16:20 +1000

    112 ac

--- 112/112_Path Sum.py
+++ 112/112_Path Sum.py
@@ -5,6 +5,8 @@
 #         self.left = None
 #         self.right = None
 
+import util.BinaryTree as bt
+
 class Solution(object):
     def hasPathSum(self, root, sum):
         """
@@ -12,8 +14,43 @@ class Solution(object):
         :type sum: int
         :rtype: bool
         """
+        self.sumFromRoot(root)
+        return self.isValInLeaf(root, sum)
 
 
+    def sumFromRoot(self, root):
+        if root is None or root.val is None:
+            return
+        if root.left is not None:
+            root.left.val += root.val
+            self.sumFromRoot(root.left)
+
+        if root.right is not None:
+            root.right.val += root.val
+            self.sumFromRoot(root.right)
+
+    def isLeaf(self, node):
+        return node is not None and node.left is None and node.right is None
+
+    def isValInLeaf(self, root, val):
+        if root is None or root.val is None:
+            return False
+        elif self.isLeaf(root):
+            if root.val == val:
+                return True
+            else:
+                return False
+        else:
+            return self.isValInLeaf(root.left, val) or self.isValInLeaf(root.right, val)
+
+
+def test_solution(l, sum):
+    test = Solution()
+    tree = bt.BinaryTree(l)
+    print 'tree =', tree.tolist()
+    print test.hasPathSum(tree.root, sum)
+
+test_solution([5,4,8,11,None,13,4,7,2,None,None,None,1], 22)
 # Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 #
 # Note: A leaf is a node with no children.
