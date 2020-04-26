689e715288323bdf69aa6b93caf767de463ffc79
Fan Zeng <fanzengau@gmail.com>
2020-04-26 23:14:49 +1000

    1143 DP recursive version MLE

---
+++ 1143/1143_Longest_Common_Subsequence.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        self.d = {}
+        return self.longestCommonSubsequenceDP(text1, text2)
+
+    def longestCommonSubsequenceDP(self, text1, text2):
+        if text1 is None or text2 is None or len(text1) == 0 or len(text2) == 0:
+            return 0
+        if self.d.get(text1 + text2) is not None:
+            return self.d.get(text1 + text2)
+        c1 = text1[0]
+        pos_c2 = text2.find(c1)
+        if pos_c2 == -1:
+            res_include = 0
+        else:
+            if len(text1) > 1:
+                remain_1 = text1[1:]
+            else:
+                remain_1 = None
+            if len(text2) > pos_c2+1:
+                remain_2 = text2[pos_c2+1:]
+            else:
+                remain_2 = None
+            res_include = 1 + self.longestCommonSubsequenceDP(remain_1, remain_2)
+        res_not_include = self.longestCommonSubsequenceDP(text1[1:], text2)
+        res = max(res_include, res_not_include)
+        self.d[text1 + text2] = res
+        return res
+
+
+test = Solution()
+print test.longestCommonSubsequence("abdefghij", "adbaceghj") # 6
+print test.longestCommonSubsequence("abcde", "ace") # 3
+print test.longestCommonSubsequence("abc", "abc") # 3
+print test.longestCommonSubsequence("abc", "def") # 0
+
+print test.longestCommonSubsequence(
+    "whydynkfslsrmvyhonyjenyrenojofafmnafmfyhyjebwhqpwh",
+    "ctqdkfctanypmxqxktqfwfgnwjqpsbgpydovufgfqbyvqpufujy"
+)
+print test.longestCommonSubsequence(
+    "fcvafurqjylclorwfoladwfqzkbebslwnmpmlkbezkxoncvwhstwzwpqxqtyxozkpgtgtsjobujezgrkvevklmludgtyrmjaxyputqbyxqvupojutsjwlwluzsbmvyxifqtglwvcnkfsfglwjwrmtyxmdgjifyjwrsnenuvsdedsbqdovwzsdghclcdexmtsbexwrszihcpibwpidixmpmxshwzmjgtadmtkxqfkrsdqjcrmxkbkfoncrcvoxuvcdytajgfwrcxivixanuzerebuzklyhezevonqdsrkzetsrgfgxibqpmfuxcrinetyzkvudghgrytsvwzkjulmhanankxqfihenuhmfsfkfepibkjmzybmlkzozmluvybyzsleludsxkpinizoraxonmhwtkfkhudizepyzijafqlepcbihofepmjqtgrsxorunshgpazovuhktatmlcfklafivivefyfubunszyvarcrkpsnglkduzaxqrerkvcnmrurkhkpargvcxefovwtapedaluhclmzynebczodwropwdenqxmrutuhehadyfspcpuxyzodifqdqzgbwhodcjonypyjwbwxepcpujerkrelunstebopkncdazexsbezmhynizsvarafwfmnclerafejgnizcbsrcvcnwrolofyzulcxaxqjqzunedidulspslebifinqrchyvapkzmzwbwjgbyrqhqpolwjijmzyduzerqnadapudmrazmzadstozytonuzarizszubkzkhenaxivytmjqjgvgzwpgxefatetoncjgjsdilmvgtgpgbibexwnexstipkjylalqnupexytkradwxmlmhsnmzuxcdkfkxyfgrmfqtajatgjctenqhkvyrgvapctqtyrufcdobibizihuhsrsterozotytubefutaxcjarknynetipehoduxyjstufwvkvwvwnuletybmrczgtmxctuny",
+    "nohgdazargvalupetizezqpklktojqtqdivcpsfgjopaxwbkvujilqbclehulatshehmjqhyfkpcfwxovajkvankjkvevgdovazmbgtqfwvejczsnmbchkdibstklkxarwjqbqxwvixavkhylqvghqpifijohudenozotejoxavkfkzcdqnoxydynavwdylwhatslyrwlejwdwrmpevmtwpahatwlaxmjmdgrebmfyngdcbmbgjcvqpcbadujkxaxujudmbejcrevuvcdobolcbstifedcvmngnqhudixgzktcdqngxmruhcxqxypwhahobudelivgvynefkjqdyvalmvudcdivmhghqrelurodwdsvuzmjixgdexonwjczghalsjopixsrwjixuzmjgxydqnipelgrivkzkxgjchibgnqbknstspujwdydszohqjsfuzstyjgnwhsrebmlwzkzijgnmnczmrehspihspyfedabotwvwxwpspypctizyhcxypqzctwlspszonsrmnyvmhsvqtkbyhmhwjmvazaviruzqxmbczaxmtqjexmdudypovkjklynktahupanujylylgrajozobsbwpwtohkfsxeverqxylwdwtojoxydepybavwhgdehafurqtcxqhuhkdwxkdojipolctcvcrsvczcxedglgrejerqdgrsvsxgjodajatsnixutihwpivihadqdotsvyrkxehodybapwlsjexixgponcxifijchejoxgxebmbclczqvkfuzgxsbshqvgfcraxytaxeviryhexmvqjybizivyjanwxmpojgxgbyhcruvqpafwjslkbohqlknkdqjixsfsdurgbsvclmrcrcnulinqvcdqhcvwdaxgvafwravunurqvizqtozuxinytafopmhchmxsxgfanetmdcjalmrolejidylkjktunqhkxchyjmpkvsfgnybsjedmzkrkhwryzan"
+)
