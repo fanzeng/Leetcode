d6e0a20b5c75a09be919a7c17f87d38f6ec08507
Fan Zeng <fanzengau@gmail.com>
2020-04-27 16:21:02 +1000

    1143 ac

--- 1143/1143_Longest_Common_Subsequence.py
+++ 1143/1143_Longest_Common_Subsequence.py
@@ -10,27 +10,62 @@ class Solution(object):
 
     def longestCommonSubsequenceDP(self, text1, text2):
         if text1 is None or text2 is None or len(text1) == 0 or len(text2) == 0:
-            return 0
-        if self.d.get(text1 + text2) is not None:
-            return self.d.get(text1 + text2)
-        c1 = text1[0]
-        pos_c2 = text2.find(c1)
-        if pos_c2 == -1:
-            res_include = 0
-        else:
-            if len(text1) > 1:
-                remain_1 = text1[1:]
-            else:
-                remain_1 = None
-            if len(text2) > pos_c2+1:
-                remain_2 = text2[pos_c2+1:]
-            else:
-                remain_2 = None
-            res_include = 1 + self.longestCommonSubsequenceDP(remain_1, remain_2)
-        res_not_include = self.longestCommonSubsequenceDP(text1[1:], text2)
-        res = max(res_include, res_not_include)
-        self.d[text1 + text2] = res
-        return res
+            return  0
+        self.d[len(text1)-1, len(text2)-1] = int(text1[-1] == text2[-1])
+        for j in xrange(len(text2) - 2, -1, -1):
+            self.d[len(text1)-1, j] = max(self.d[len(text1)-1, j+1], int(text1[-1] == text2[j]))
+        for i in xrange(len(text1)-2, -1, -1):
+            self.d[i, len(text2)-1] = max(self.d[i+1, len(text2)-1], int(text1[i] == text2[-1]))
+            for j in xrange(len(text2)-2, -1, -1):
+                if text2[j] == text1[i]:
+                    res = self.d[i+1, j+1] + 1
+                    # there is no need to max it with self.d[i, j+1] and self.d[i+1, j],
+                    # because when text2[j] == text1[i],
+                    # the max possible value of self.d[i, j] is self.d[i+1, j+1] + 1, i.e., you can gain at most 1 from there
+                    # and this value is always achievable by consuming the char in both strings,
+                    # so no need for more conditions, just simply add 1.
+
+                    # the reason why you can only gain at most 1:
+                    # if otherwise, the only possible way to gain 2 is for text2[j] to match some char in text[i+1:],
+                    # and text1[i] to match some char in text2[j+1:],
+                    # since text2[j] == text1[i], all these 4 chars are the same char
+                    # so we have at most 2 pairs, from which we can gain at most 2.
+                    # but that gain of 1 from the other pair has already been counted in
+                    # self.d[i+1, j+1] by definition of this algorithm,
+                    # if you look at this algorithm, when i and j match the location of those two chars, that 1 has been
+                    # added right here on this very line.
+                    # so in order not to double count, we should gain at most 1 here,
+                    # and this is done by matching text1[i] and text2[j].
+                else:
+                    res = max(self.d[i+1, j], self.d[i, j+1])
+                self.d[i, j] = res
+                # print i, j, res
+
+        return self.d[0, 0]
+# recursive version, does not work for long strings: maximum recursion depth exceeded
+    # def longestCommonSubsequenceDP(self, text1, text2):
+    #     if text1 is None or text2 is None or len(text1) == 0 or len(text2) == 0:
+    #         return 0
+    #     if self.d.get(text1 + text2) is not None:
+    #         return self.d.get(text1 + text2)
+    #     c1 = text1[0]
+    #     pos_c2 = text2.find(c1)
+    #     if pos_c2 == -1:
+    #         res_include = 0
+    #     else:
+    #         if len(text1) > 1:
+    #             remain_1 = text1[1:]
+    #         else:
+    #             remain_1 = None
+    #         if len(text2) > pos_c2+1:
+    #             remain_2 = text2[pos_c2+1:]
+    #         else:
+    #             remain_2 = None
+    #         res_include = 1 + self.longestCommonSubsequenceDP(remain_1, remain_2)
+    #     res_not_include = self.longestCommonSubsequenceDP(text1[1:], text2)
+    #     res = max(res_include, res_not_include)
+    #     self.d[text1 + text2] = res
+    #     return res
 
 
 test = Solution()
