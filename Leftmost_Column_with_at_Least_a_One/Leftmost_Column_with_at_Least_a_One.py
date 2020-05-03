6780ec4c6af2572064d789e8dd045329099e3abc
Fan Zeng <fanzengau@gmail.com>
2020-05-03 23:30:26 +1000

    Leftmost_Column_with_at_Least_a_One ac

--- Leftmost_Column_with_at_Least_a_One/Leftmost_Column_with_at_Least_a_One.py
+++ Leftmost_Column_with_at_Least_a_One/Leftmost_Column_with_at_Least_a_One.py
@@ -1,10 +1,67 @@
-import unittest
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
 
+class BinaryMatrix(object):
+    def __init__(self, mat):
+        self.mat = mat
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def get(self, x, y):
+       """
+       :type x : int, y : int
+       :rtype int
+       """
+       return self.mat[x][y]
 
+    def dimensions(self):
+       """
+       :rtype list[]
+       """
+       return [len(self.mat), len(self.mat[0])]
 
-if __name__ == '__main__':
-    unittest.main()
+class Solution(object):
+    def leftMostColumnWithOne(self, binaryMatrix):
+        """
+        :type binaryMatrix: BinaryMatrix
+        :rtype: int
+        """
+        rows = binaryMatrix.dimensions()[0]
+        cols = binaryMatrix.dimensions()[1]
+        min_c = cols
+        for r in xrange(rows):
+            min_c_candidate = self.leftMostColumnWithOneSingleRow(binaryMatrix, r, min_c, cols)
+            if min_c_candidate != -1:
+                min_c = min(min_c, min_c_candidate)
+        if min_c < cols:
+            return min_c
+        else:
+            return -1
+
+    def leftMostColumnWithOneSingleRow(self, binaryMatrix, row, col, cols):
+        l = 0
+        r = min(col, cols-1)
+        while l < r-1:
+            m = (l + r) / 2
+            if binaryMatrix.get(row, m) == 1:
+                r = m
+                m = (l + r) / 2
+            else:
+                l = m
+                m = (l + r) / 2
+        if binaryMatrix.get(row, l) == 1:
+            return l
+        if binaryMatrix.get(row, r) == 1:
+            return r
+        else:
+            return -1
+
+mat1 = [[0,0],[1,1]]
+mat2 = [[0,0],[0,1]]
+mat3 = [[0,0],[0,0]]
+mat4 = [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+test = Solution()
+print test.leftMostColumnWithOne(BinaryMatrix(mat1))
+print test.leftMostColumnWithOne(BinaryMatrix(mat2))
+print test.leftMostColumnWithOne(BinaryMatrix(mat3))
+print test.leftMostColumnWithOne(BinaryMatrix(mat4))
