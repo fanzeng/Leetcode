a4a62842b938147786da1dfd256cda91aaf5415b
Fan Zeng <fanzengau@gmail.com>
2020-06-13 12:15:06 +1000

    771 ac

---
+++ 771/771_Jewels_and_Stones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for s in S:
+            if s in J:
+                count += 1
+        return count
+
+test = Solution()
+print test.numJewelsInStones("aA", "aAAbbbb")
+print test.numJewelsInStones("z", "ZZ")
