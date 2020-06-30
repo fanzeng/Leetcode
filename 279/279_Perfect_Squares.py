94d942fe36d974d7216d6ebd233111e9ad97e59c
Fan Zeng <fanzengau@gmail.com>
2020-06-30 23:41:07 +1000

    279 ac

--- 279/279_Perfect_Squares.py
+++ 279/279_Perfect_Squares.py
@@ -5,39 +5,42 @@ class Solution(object):
         :rtype: int
         """
         self.d = {1:1, 2:2, 3:3, 4:1}
-        return self.numSquaresDP(n)
+        self.numSquaresDP(n)
+        return self.d[n]
 
 
     def numSquaresDP(self, n):
         d_res = self.d.get(n)
         if d_res is not None:
             return d_res
-        if self.is_sq(n):
-            return 1
-        l_res = []
-        m = 1
-        sq = m*m
-        while n - sq >= n/2:
-            r = 1 + self.numSquaresDP(n - sq)
-            l_res.append(r)
-            if r < 3:
-                break
-            m += 1
+        for i in xrange(5, n+1):
+            l_res = []
+            m = self.sqrtFloor(i)
+            if m*m == i:
+                self.d[i] = 1
+                continue
             sq = m*m
-        # print n, l_res
-        res = min(l_res)
-        self.d[n] = res
-        return res
+            while sq > 0:
+                # print i, i-sq
+                r = 1 + self.d.get(i - sq)
+                l_res.append(r)
+                if r < 3:
+                    break
+                m -= 1
+                sq = m*m
+            # print i, l_res
+            res = min(l_res)
+            self.d[i] = res
 
-    def is_sq(self, n):
+    def sqrtFloor(self, n):
         m = 1
         while m*m <= n:
             m += 1
-        return (m-1)*(m-1) == n
+        return m - 1
 
 
 test = Solution()
 print test.numSquares(12) # 3
 print test.numSquares(13) # 2
 print test.numSquares(477) # 2
-print test.numSquares(7168) # 2
+print test.numSquares(7168) # 4
