62c22d2f22b26a8470c6f0754042b680455dda9f
Fan Zeng <fanzengau@gmail.com>
2020-08-26 17:23:07 +1000

    497 ac

--- 497/497_Random_Point_in_Non_overlapping_Rectangles.py
+++ 497/497_Random_Point_in_Non_overlapping_Rectangles.py
@@ -1,25 +1,45 @@
+from random import randint
 class Solution(object):
-
     def __init__(self, rects):
         """
         :type rects: List[List[int]]
         """
         self.d = {}
-        i = 0
+        self.rects = rects
+        self.total_area = 0
+        self.accu_area = []
         for rect in rects:
-            for r in xrange(rect[0], rect[2]+1):
-                for c in xrange(rect[1], rect[3]+1):
-                    self.d[i] = [r, c]
-                    i += 1
-        # print self.d
-        self.total = i-1
+            area = (rect[2]-rect[0]+1)*(rect[3]-rect[1]+1)
+            self.total_area += area
+            self.accu_area.append(self.total_area)
+        # print self.accu_area
+
+    def pickPointFromRect(self, rect, remainder):
+        remainder -= 1
+        w = rect[2] - rect[0] + 1
+        if w == 0:
+            row = 0
+            col = remainder
+        else:
+            row = remainder / w
+            col = remainder % w
+        x = rect[0] + col
+        y = rect[1] + row
+        return [x, y]
 
     def pick(self):
         """
         :rtype: List[int]
         """
-        from random import randint
-        return self.d[randint(0, self.total)]
+        rand_num = randint(1, self.total_area)
+        rect_id = 0
+        while self.accu_area[rect_id] < rand_num:
+            rect_id += 1
+        remainder = rand_num
+        if rect_id > 0:
+            remainder = rand_num - self.accu_area[rect_id-1]
+        # print rect_id, remainder
+        return self.pickPointFromRect(self.rects[rect_id], remainder)
 
 obj = Solution([[1,1,5,5]])
 print obj.pick()
