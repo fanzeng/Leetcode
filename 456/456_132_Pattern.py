cb2475ac3e56fe0d11fd4bcc82cfb42fe5f5df13
Fan Zeng <fanzengau@gmail.com>
2020-11-03 12:15:45 +1000

    456 ac slightly better

--- 456/456_132_Pattern.py
+++ 456/456_132_Pattern.py
@@ -11,19 +11,22 @@ class Solution(object):
         nums = nums_no_repeat
         if len(nums) < 3:
             return False
-        i = 2
+        i = 1
         # go through nums with each num as candidate for "2"
         # let "3" be the rightmost position of numbers that are greater than num and on the left of num
         # let "1" be the leftmost position of numbers that are smaller than num and on the left of num
         # if leftmost_smaller < rightmost_greater, return True
         running_max = nums[0]
         running_min = nums[0]
+        d_first_below = {nums[0]+1: 0}
         while i < len(nums):
             if nums[i] > running_max:
                 running_max = nums[i]
                 continue
             if nums[i] < running_min:
                 running_min = nums[i]
+                if d_first_below.get(nums[i]+1) is None:
+                    d_first_below[nums[i]+1] = i
                 continue
 
             j = i - 1
@@ -33,14 +36,13 @@ class Solution(object):
                     greater = j
                     break
                 j -= 1
-
-            k = 0
-            while k < greater:
-                if nums[k] < nums[i]:
+            l_key = [key for key in d_first_below.keys() if key <= nums[i]]
+            # print i, nums[i], d_first_below, l_key
+            if len(l_key) > 0:
+                k = min([d_first_below[key] for key in l_key])
+                if k < greater:
                     return True
-                k += 1
             i += 1
-
         return False
 
 test = Solution()
