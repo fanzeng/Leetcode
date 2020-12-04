ba8206288a74c4c80484eae76f89eb8537d8051c
Fan Zeng <fanzengau@gmail.com>
2020-12-04 21:56:18 +1000

    1492 ac

--- 1492/1492_The_kth_Factor_of_n.py
+++ 1492/1492_The_kth_Factor_of_n.py
@@ -5,28 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        self.set_prime = set(range(2, n+1))
-        self.l_factor = []
-        while n > 1:
-            p = self.set_prime.pop()
-            n = self.tryPrime(n, p)
-        print self.l_factor
-        return self.l_factor[k] if k < len(self.l_factor) else -1
-    
-    def tryPrime(self, n, p):
-        m = 2*p
-        while m <= n:
-            self.set_prime.discard(m)
-            m += p
-        if n % p == 0:
-            self.l_factor.append(p)
-        while n > 1 and n % p == 0:
-            n /= p
-        return n
+        count = 0
+        for f in xrange(1, n+1):
+            if n % f == 0:
+                count += 1
+                if count == k:
+                    return f
+        return -1
 
 test = Solution()
-print test.kthFactor(12, 3)
-print test.kthFactor(7, 2)
+print test.kthFactor(12, 3) # 3
+print test.kthFactor(7, 2) # 7
 print test.kthFactor(4, 4) # -1
-print test.kthFactor(1, 1)
-print test.kthFactor(1000, 3)
+print test.kthFactor(1, 1) # 1
+print test.kthFactor(1000, 3) # 4
