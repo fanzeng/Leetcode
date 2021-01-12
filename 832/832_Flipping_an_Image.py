56791fa89e42e74f6a8daf44467bf38a39e3aae1
Fan Zeng <fanzengau@gmail.com>
2021-01-12 22:27:54 +1000

    832 ac

---
+++ 832/832_Flipping_an_Image.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[1-pix for pix in reversed(row)] for row in A]
