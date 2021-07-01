d6fb3654ffcba7d527ce28556f372cf894a6974a
Fan Zeng <fanzengau@gmail.com>
2021-07-01 18:45:52 +1000

    239 heap v4

--- 239/239_Sliding_Window_Maximum.py
+++ 239/239_Sliding_Window_Maximum.py
@@ -8,23 +8,23 @@ class Solution(object):
         if k >= len(nums):
             return [max(nums)]
         res = [None]*(len(nums)-k+1)
-        res[0] = max(nums[:k])
-        self.heap = MaxHeap(len(nums), k)
+        self.heap = MaxHeap(len(nums))
         for num in nums[:k]:
-            self.heap.popInsert(num)
+            self.heap.insert(num)
+        res[0] = self.heap.val[0]
 
         i = 1
         for num in nums[k:]:
-            self.heap.popInsert(num)
+            self.heap.pop(nums[i-1])
+            self.heap.insert(num)
             res[i] = self.heap.val[0]
             i += 1
         return res
 
 class Heap(object):
-    def __init__(self, isGreater, sz=1, k=1):
+    def __init__(self, isGreater, sz=1):
         self.val = [None]*sz
-        self.k = k
-        self.recent = []
+        self.d = {}
         self.size = 0
         self.isGreater = isGreater
 
@@ -35,22 +35,16 @@ class Heap(object):
         return 2*i + 1, 2*i + 2
 
     def swap(self, i, j):
-        if i == j:
-            return
-        i_new = None
-        j_new = None
-        if i in self.recent:
-            i_old_id = self.recent.index(i)
-            i_new = j
-        if j in self.recent:
-            j_old_id = self.recent.index(j)
-            j_new = i
-        if i_new is not None:
-            # print 'self.recent[', i_old_id, '] =', i_new
-            self.recent[i_old_id] = i_new
-        if j_new is not None:
-            # print 'self.recent[', j_old_id, '] =', j_new
-            self.recent[j_old_id] = j_new
+        try:
+            di = self.d[self.val[i]]
+            di[di.index(i)] = j
+        except:
+            pass
+        try:
+            dj = self.d[self.val[j]]
+            dj[dj.index(j)] = i
+        except:
+            pass
 
         temp = self.val[i]
         self.val[i] = self.val[j]
@@ -66,11 +60,12 @@ class Heap(object):
 
     def insert(self, val):
         self.size += 1
-        if len(self.val) >= self.size:
-            self.val[self.size-1] = val
-        else:
-            self.val.append(val)
+        self.val[self.size - 1] = val
         i = self.float(self.size-1)
+        if self.d.get(val) is None:
+            self.d[val] = [i]
+        else:
+            self.d[val].append(i)
         return i
 
     def sink(self, i):
@@ -88,56 +83,6 @@ class Heap(object):
             i = self.sink(smaller_child_id)
         return i
 
-    def pop(self):
-        if len(self.val) == 0:
-            return None
-        root = self.val[0]
-        self.swap(0, self.size-1)
-        self.size -= 1
-        i = self.sink(0)
-        return root
-
-    # dev helper function to verify whether self is still a heap
-    def _isHeap(self, i):
-        if i >= self.size:
-            return False
-        children_id = self.child(i)
-        if children_id[0] < self.size:
-            if self.isGreater(self.val[i], self.val[children_id[0]]):
-                return False
-            is_first_child_heap = self._isHeap(children_id[0])
-        else:
-            is_first_child_heap = True
-        if children_id[1] < self.size:
-            if self.isGreater(self.val[i], self.val[children_id[1]]):
-                return False
-            is_second_child_heap = self._isHeap(children_id[1])
-        else:
-            is_second_child_heap = True
-        return is_first_child_heap and is_second_child_heap
-
-    def _find(self, val, start):
-        if start >= self.size:
-            return None
-        if self.val[start] == val:
-            return start
-        children_id = self.child(start)
-        if children_id[0] < self.size:
-            if self.val[children_id[0]] == val:
-                return children_id[0]
-            elif self.isGreater(val, self.val[children_id[0]]):
-                first = self._find(val, children_id[0])
-                if first:
-                    return first
-        if children_id[1] < self.size:
-            if self.val[children_id[1]] == val:
-                return children_id[1]
-            elif self.isGreater(val, self.val[children_id[1]]):
-                return self._find(val, children_id[1])
-
-    def find(self, val):
-        return self._find(val, 0)
-
     def popAt(self, i):
         val = self.val[i]
         if i == self.size-1:
@@ -153,27 +98,13 @@ class Heap(object):
         return val
 
     def pop(self, val):
-        if len(self.val) == 0:
-            return None
-        i = self.find(val)
-        if i is None:
-            print 'Error poping', val, ', does not exist in the heap.'
-            return None
+        i = self.d[val].pop()
         self.popAt(i)
 
-    def popInsert(self, val):
-        if len(self.recent)+1 > self.k:
-            j = self.recent.pop(0)
-            self.popAt(j)
-        i = self.insert(val)
-        self.recent.append(i)
-        # print 'recently inserted', [self.val[pos] for pos in self.recent]
-        return i
-
 class MaxHeap(Heap):
-    def __init__(self, sz=1, k=1):
+    def __init__(self, sz=1):
         isGreater = lambda a, b: a < b
-        super(MaxHeap, self).__init__(isGreater, sz, k)
+        super(MaxHeap, self).__init__(isGreater, sz)
 
 
 test = Solution()
