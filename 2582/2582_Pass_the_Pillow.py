aeae1b9a5bb6cbeee08ff039948ded5a5f547b21
Fan Zeng <fanzengau@gmail.com>
2024-07-06 23:57:05 +1000

    Update 2582_Pass_the_Pillow.py

--- 2582/2582_Pass_the_Pillow.py
+++ 2582/2582_Pass_the_Pillow.py
@@ -6,8 +6,4 @@ class Solution(object):
         :rtype: int
         """
         # every n - 1 steps it reaches one of the end points
-        direction = time / (n - 1)
-        if direction % 2 == 0: # left to right
-            return 1 + time % (n - 1)
-        else:
-            return n - time % (n - 1
+        return 1 + time % (n - 1) if (time / (n - 1)) % 2 == 0 else n - time % (n - 1)
