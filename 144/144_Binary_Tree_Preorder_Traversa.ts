abc1262dfe2ab4340c3b47f6368b18755ca4bdf4
Fan Zeng <fanzengau@gmail.com>
2024-10-07 15:26:07 +1000

    Create 144_Binary_Tree_Preorder_Traversa.ts

---
+++ 144/144_Binary_Tree_Preorder_Traversa.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    if (root == null) {
+        return [];
+    }
+    const left = preorderTraversal(root.left);
+    const right = preorderTraversal(root.right);
+    left.unshift(root.val);
+    return left.concat(right);
+};
