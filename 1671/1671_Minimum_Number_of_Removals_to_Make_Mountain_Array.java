97dc424c0aaed9333f5e6b78f42ad621d2bf210d
Fan Zeng <fanzengau@gmail.com>
2024-11-01 00:05:31 +1000

    Update 1671_Minimum_Number_of_Removals_to_Make_Mountain_Array.java

--- 1671/1671_Minimum_Number_of_Removals_to_Make_Mountain_Array.java
+++ 1671/1671_Minimum_Number_of_Removals_to_Make_Mountain_Array.java
@@ -1,44 +1,50 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
+        int[] lisLR = lis(nums);
+        int[] rev = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            rev[i] = nums[nums.length-1-i];
+        }
+        int[] lisRL = lis(rev);
         int res = nums.length;
         for (int i = 1; i < nums.length-1; i++) {
-            int curr = nums[i];
-            int[] left = Arrays.stream(nums, 0, i)
-                .filter(n -> n < curr)
-                .toArray();
-            if (left.length == 0) continue;
-            int[] right = Arrays.stream(nums, i+1, nums.length)
-                .filter(n -> n < curr)
-                .boxed()
-                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
-                    Collections.reverse(list);
-                    return list.stream();
-                }))
-                .mapToInt(Integer::intValue)
-                .toArray();
-            if (right.length == 0) continue;
-            int lisLeft = lis(left);
-            int lisRight = lis(right);
-            // System.out.printf("i = %d, left = %d, right = %d\n", i, lisLeft, lisRight);
-            int ans = nums.length - (lisLeft + lisRight + 1);
+            int lr = lisLR[i];
+            int rl = lisRL[nums.length-1-i];
+            // System.out.printf("i:%d, LR: %d, RL: %d\n", i, lr, rl);
+            // We need at least 2 numbers including the peak to be valid.
+            if (lr <= 1 || rl <= 1) continue;
+            int ans = nums.length - (lr + rl - 1);
             if (ans < res) res = ans;
         }
         return res;
     }
-    private int lis(int[] nums) {
+    // Returns lengths of lis at each index
+    private int[] lis(int[] nums) {
+        int[] res = new int[nums.length];
         int[] lis = new int[nums.length];
+        res[0] = 1;
         lis[0] = nums[0];
         int length = 1;
         for (int i = 1; i < nums.length; i++) {
             int n = nums[i];
             if (n > lis[length-1]) {
                 lis[length++] = n;
-            } else {
+                res[i] = length;
+            } else if (n == lis[length-1]) {
+                res[i] = length;
+            }
+            else {
                 int j = binarySearch(lis, -1, length-1, n);
                 lis[j] = n;
+                // Whenever we read this res[i] result,
+                // we're assuming the ith number is the peak.
+                // So for this particular problem,
+                // if we end up here, we know res[i] is not the peak,
+                // and we need to make res[i] invalid.
+                // Here we do not update res[i], so it's left at default value of 0.
             }
         }
-        return length;
+        return res;
     }
     private int binarySearch(int[] arr, int l, int r, int n) {
         while (r - l > 1) {
