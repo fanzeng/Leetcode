c349f91e601404a6d66a13ae993575f142747234
Fan Zeng <fanzengau@gmail.com>
2024-11-22 23:59:40 +1000

    Update 1072_Flip_Columns_for_Maximum_Number_of_Equal_Rows.java

--- 1072/1072_Flip_Columns_for_Maximum_Number_of_Equal_Rows.java
+++ 1072/1072_Flip_Columns_for_Maximum_Number_of_Equal_Rows.java
@@ -1,50 +1,37 @@
 class Solution {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
-        return dp(matrix, 0);
-    }
-    private int dp(int[][] m, int r) {
-        // For each row, we either give up or not give up.
-        // If not give up, we either flip all 0 to 1 or all 1 to 0.
-        // Whatever we choose to do, we have constraint for the subsequent rows.
-
-        // Result of discarding the row.
-        int giveUp;
-        if (r == m.length-1) {
-            giveUp = 0;
-        } else {
-            giveUp = dp(m, r+1);
-        } 
-        // Result of keeping the row as all 0.
-        int zero = 1 + check(m, r+1, m[r]);
-        int[] flipped = new int[m[0].length]; // Flipped cols. 0 - not flipped, 1 - flipped.
-        for (int j = 0; j < m[0].length; j++) {
-            flipped[j] = 1 - m[r][j];
+        if (matrix.length == 1) return 1;
+        int[] visited = new int[matrix.length];
+        int res = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            if (visited[i] > 0) continue;
+            int count = 1;
+            for (int j = i+1; j < matrix.length; j++) {
+                if (isSameOrComplement(matrix[i], matrix[j])) {
+                    count++;
+                    visited[j] = 1;
+                }
+            }
+            if (count > res) res = count;
+            if (res >= matrix.length - i - 1) break;
         }
-        // Result of keeping the row as all 1.
-        int one = 1 + check(m, r+1, flipped);
-        return Math.max(zero, Math.max(giveUp, one));
+        return res;
     }
-
-    // Given flipped cols, return "number of equal rows"
-    private int check(int[][] m, int r, int[] flipped) {
-        if (r == m.length) return 0;
-        int rest;
-        if (r == m.length-1) {
-            rest = 0;
-        } else {
-            rest = check(m, r+1, flipped);
-        }
-        boolean ok = true;
-        int e = m[r][0];
-        if (flipped[0] == 1) e = 1 - e;
-        for (int j = 1; j < flipped.length; j++) {
-            int v = m[r][j];
-            if (flipped[j] == 1) v = 1 - v;
-            if (v != e) {
-                ok = false;
+    private boolean isSameOrComplement(int[] a, int[] b) {
+        boolean isSame = true;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                isSame = false;
                 break;
             }
         }
-        return ok ? rest+1 : rest;
+        boolean isComplement = true;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) {
+                isComplement = false;
+                break;
+            }
+        }
+        return isSame || isComplement;
     }
 }
